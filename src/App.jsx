437d2f96fca402e9f98ea785be6e91c36ec5cfd9
sheinDihayco <shiendihayco16@gmail.com>
2024-07-16 20:22:16 +0800

    finalTask1

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+
+  return (
+    <div className="App">
+
+      <h1>First React App Installation</h1>
+      <h3>What is React?</h3>
+      <p>
+          React is a popular JavaScript library developed by Facebook for building user interfaces, especially single-page applications. It uses a component-based architecture, allowing developers to create reusable UI components. With JSX syntax, developers can write HTML-like code within JavaScript, making UI code more readable. React's virtual DOM ensures efficient updates and rendering, enhancing performance. Its unidirectional data flow and declarative approach make the code predictable and easier to debug.
+      </p>
+
+      <ol>
+        <li>Dihayco, Sheinalie V.</li>
+        <li>Tahanlangit, Louie A.</li>
+      </ol>
+
+      <p>
+        <b>Note:</b> React is widely adopted with a rich ecosystem and strong community support.
+      </p>
+    </div>
+  )
+}
+
+export default App;
